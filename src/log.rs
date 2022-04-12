08a73445d58b63d3fe1937101b5883ad6b15bd08
mochaaP <git@mochaa.ws>
2022-04-12 14:13:45 +0800

    chore: bump deps, remove lockfile
    chore: bump version

--- src/log.rs
+++ src/log.rs
@@ -1,16 +1,12 @@
 extern crate flexi_logger;
-extern crate xdg;
-
 use flexi_logger::{opt_format, Logger};
 
 pub fn init() {
-  let xdg_dirs = xdg::BaseDirectories::with_prefix("figma-fonthelper").unwrap();
 
-  Logger::with_env_or_str(
+  Logger::try_with_env_or_str(
     "font_helper=debug, finder=debug, libfonthelper=debug, simple_server=info",
   )
-  .log_to_file()
-  .directory(xdg_dirs.get_cache_home())
+  .unwrap()
   .format(opt_format)
   .start()
   .unwrap();
